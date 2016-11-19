59bfc4ff1cac2344f08ed84160dcff5fc105c809
Abhinav Mishra <abhinav.mishra15@imperial.ac.uk>
2016-11-19 16:55:08 +0000

    Add base server

--- main.go
+++ main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+var currentFeedNumber = 0
+
+type NewVideoFeed struct {
+	ID     int
+	SQSUrl string
+	S3Url  string
+}
+
+func returnNewVideoStreamID(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Requested /new")
+
+	nvf := NewVideoFeed{
+		ID:     currentFeedNumber,
+		SQSUrl: "You will get the SQS Url here",
+		S3Url:  "You will get the S3 url here",
+	}
+
+	json.NewEncoder(w).Encode(nvf)
+	currentFeedNumber++
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "3000"
+	}
+
+	http.HandleFunc("/new", returnNewVideoStreamID) // set router
+
+	fmt.Printf("Go server started on port %v\n", port)
+
+	err := http.ListenAndServe(":"+port, nil) // set listen port
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
